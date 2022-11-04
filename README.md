b0e0f0b9d95c0f63c0f3fbcd75bab24139bc4561
JoshuaToback <joshuatoback@gmail.com>
2022-11-03 21:40:56 -0400

    Added README and SearchHistory Functions

---
+++ README.md
@@ -0,0 +1,30 @@
+# Weather Dashboard
+
+## Want to check your weather for the day, or your weather for the next 5 days? Look no further than this handy-dandy dashboard! You can search for any place in the world and find out how hot, windy, and humid a place is with just one input! 
+
+# Installation 
+
+## Open the Dashboard [here](https://joshuatoback.github.io/weather-dashboard/)!
+
+
+# Usage
+
+## When you first open the dashboard, you're met with an empty page with a search bar and infinite possibilities! ![EmptyScreen](assets/images/EmptyWeather.png)
+
+## Search in the bar on the left like so, and then click the button to be on your way! 
+
+## ![Search](assets/images/Search.png)
+
+## Once you hit enter, you're greeted with your results! 
+
+## ![Results](assets/images/Results.png)
+
+# Future Updates
+
+## This project is still a work in progress, but within the code there's already a few functions that need to be implemented! Here's what we have in store:
+
+## 1. Search History Container
+
+## 2. Customized Background based on Location
+
+## 3. Current Time/Date Display
--- assets/script.js
+++ assets/script.js
@@ -82,6 +82,8 @@ function fetchWeather(location) {
 
 }
 
+initSearchHistory();
+
 function renderItems(city, data) {
   renderCurrentWeather(city, data.todayForecast, data.timezone);
   renderForecast(data.daily, data.timezone);
@@ -95,6 +97,43 @@ searchForm.addEventListener('submit', function(event) {
   fetchCityCoords(city);
 })
 
+// Function to display the search history list.
+function renderSearchHistory() {
+  searchHistoryContainer.innerHTML = '';
+
+  // Start at end of history array and count down to show the most recent at the top.
+  for (var i = searchHistory.length - 1; i >= 0; i--) {
+    var btn = document.createElement('button');
+    btn.setAttribute('type', 'button');
+    btn.setAttribute('aria-controls', 'today forecast');
+    btn.classList.add('history-btn', 'btn-history');
+
+    // `data-search` allows access to city name when click handler is invoked
+    btn.setAttribute('data-search', searchHistory[i]);
+    btn.textContent = searchHistory[i];
+    searchHistoryContainer.append(btn);
+  }
+}
+
+// Function to update history in local storage then updates displayed history.
+function appendToHistory(search) {
+  // If there is no search term return the function
+  if (searchHistory.indexOf(search) !== -1) {
+    return;
+  }
+  searchHistory.push(search);
+
+  localStorage.setItem('search-history', JSON.stringify(searchHistory));
+  renderSearchHistory();
+}
+
+function initSearchHistory() {
+  var storedHistory = localStorage.getItem('search-history');
+  if (storedHistory) {
+    searchHistory = JSON.parse(storedHistory);
+  }
+  renderSearchHistory();
+}
 
 function renderCurrentWeather(city, todayForecast, timezone) {
 //  API Variables
@@ -103,14 +142,12 @@ function renderCurrentWeather(city, todayForecast, timezone) {
 
 
   var top = document.createElement('div');
-  var topTitle = document.createElement('h4');
+
 
   todayContainer.innerHTML = '';
   todayContainer.append(top);
 
   top.setAttribute('class', 'col-lg-9 pb-3')
-  topTitle.textContent = 'Current Forecast:';
-  top.append(topTitle);
 
   todayContainer.innerHTML = '';
   todayContainer.append(top);
