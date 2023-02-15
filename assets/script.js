bbb9bb666f4ffb13804a03d5f6024c6a42f90091
JoshuaToback <joshuatoback@gmail.com>
2023-02-14 22:26:02 -0500

    Fixed duplication issue

--- assets/script.js
+++ assets/script.js
@@ -95,45 +95,8 @@ searchForm.addEventListener('submit', function(event) {
   var city = searchInput.value;
 
   fetchCityCoords(city);
+  appendToHistory(city);
 })
-
-// Function to display the search history list.
-function renderSearchHistory() {
-  searchHistoryContainer.innerHTML = '';
-
-  // Start at end of history array and count down to show the most recent at the top.
-  for (var i = searchHistory.length - 1; i >= 0; i--) {
-    var btn = document.createElement('button');
-    btn.setAttribute('type', 'button');
-    btn.setAttribute('aria-controls', 'today forecast');
-    btn.classList.add('history-btn', 'btn-history');
-
-    // `data-search` allows access to city name when click handler is invoked
-    btn.setAttribute('data-search', searchHistory[i]);
-    btn.textContent = searchHistory[i];
-    searchHistoryContainer.append(btn);
-  }
-}
-
-// Function to update history in local storage then updates displayed history.
-function appendToHistory(search) {
-  // If there is no search term return the function
-  if (searchHistory.indexOf(search) !== -1) {
-    return;
-  }
-  searchHistory.push(search);
-
-  localStorage.setItem('search-history', JSON.stringify(searchHistory));
-  renderSearchHistory();
-}
-
-function initSearchHistory() {
-  var storedHistory = localStorage.getItem('search-history');
-  if (storedHistory) {
-    searchHistory = JSON.parse(storedHistory);
-  }
-  renderSearchHistory();
-}
 
 function renderCurrentWeather(city, todayForecast, timezone) {
 //  API Variables
@@ -185,6 +148,7 @@ function renderTodayCard(city, todayForecast, timezone) {
   topInside.append(topBody);
   topBody.append(topTitle, topDate, weatherIcon, tempEl, windEl, humidityEl);
 
+  todayContainer.innerHTML = ''
   todayContainer.append(topBody)
 
 }
@@ -256,5 +220,47 @@ function renderForecastCard(forecast, timezone) {
   humidityEl.textContent = `Humidity: ${humidity} %`;
 
   forecastContainer.append(col);
+}
+
+// Function to display the search history list.
+function renderSearchHistory() {
+  searchHistoryContainer.innerHTML = '';
+
+  // Start at end of history array and count down to show the most recent at the top.
+  for (var i = searchHistory.length - 1; i >= 0; i--) {
+    var btn = document.createElement('button');
+    btn.setAttribute('type', 'button');
+    btn.setAttribute('aria-controls', 'today forecast');
+    btn.classList.add('history-btn', 'btn-history');
+
+    // `data-search` allows access to city name when click handler is invoked
+    btn.setAttribute('data-search', searchHistory[i]);
+    btn.textContent = searchHistory[i];
+    searchHistoryContainer.append(btn);
+    btn.addEventListener('click', function(e) {
+      var city = e.target.textContent
+      fetchCityCoords(city);
+    })
+  }
+}
+
+// Function to update history in local storage then updates displayed history.
+function appendToHistory(search) {
+  // If there is no search term return the function
+  if (searchHistory.indexOf(search) !== -1) {
+    return;
+  }
+  searchHistory.push(search);
+
+  localStorage.setItem('search-history', JSON.stringify(searchHistory));
+  renderSearchHistory();
+}
+
+function initSearchHistory() {
+  var storedHistory = localStorage.getItem('search-history');
+  if (storedHistory) {
+    searchHistory = JSON.parse(storedHistory);
+  }
+  renderSearchHistory();
 }
 
